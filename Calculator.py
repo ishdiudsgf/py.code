4c321a3fcf0cc9e2a372c8d40dcc2321ddaecdfd
sarvesh <guptasarvesh231@gmail.com>
2023-11-07 13:09:59 +0530

    Day1

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,7 @@
-n=int(input("Enter number:"))
-temp=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
-    print("The number is a palindrome!")
-else:
-    print("The number isn't a palindrome!")
+x=int(input("enter the number"))
+y=int(input("enter the another number"))
+sum= x+y
+sum= x-y
+sum= x*y
+sum= x/y
+print("your add number is ",sum)
---
+++ Factorial .py
@@ -0,0 +1,8 @@
+import math
+ 
+def factorial(n):
+    return(math.factorial(n))
+ 
+num = int(input("enter the number"))
+print("Factorial of", num, "is",
+      factorial(num))
